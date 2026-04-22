708bc3f1b15fff18d6f9dee2e6123c2872a5aac4
Benjamin Wassink <bwassink@wilco.org>
2026-04-22 09:17:25 -0500

    Bug: Fixed a link to the delete-file api

--- Wilco.Elections.SampleBallotLookup/wwwroot/js/site.js
+++ Wilco.Elections.SampleBallotLookup/wwwroot/js/site.js
@@ -1,6 +1,12 @@
 ﻿console.log("site.js loaded!");
 
 document.addEventListener("DOMContentLoaded", () => {
+  function buildAdminApiUrl(endpoint) {
+    const normalizedBasePath = (window.basePath || "").replace(/\/$/, "");
+    const normalizedEndpoint = endpoint.replace(/^\/+/, "");
+    return `${normalizedBasePath}/api/admin/${normalizedEndpoint}`;
+  }
+
   const electionIdInput =
     document.querySelector('input[name="ElectionId"]') ||
     document.querySelector('input[name="SelectedElectionId"]');
@@ -38,7 +44,7 @@ document.addEventListener("DOMContentLoaded", () => {
         )}&fileName=${encodeURIComponent(fileName)}`
       );
 
-      fetch("api/admin/delete-file", {
+      fetch(buildAdminApiUrl("delete-file"), {
         method: "POST",
         headers: {
           "Content-Type": "application/x-www-form-urlencoded",
@@ -87,7 +93,7 @@ document.addEventListener("DOMContentLoaded", () => {
       if (!confirm("Are you sure you want to delete all sample ballots?"))
         return;
 
-      fetch("api/admin/delete-all-sampleballots", {
+      fetch(buildAdminApiUrl("delete-all-sampleballots"), {
         method: "POST",
         headers: {
           "Content-Type": "application/x-www-form-urlencoded",
